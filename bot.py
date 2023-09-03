1aaeab7374db0d00e0ff0ccafb89420932e1832f
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-09-03 21:37:48 +0200

    second commit

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message
+
 from aiogram.utils.chat_action import ChatActionMiddleware
 
 from dotenv import load_dotenv
--- handlers/exchanger.py
+++ handlers/exchanger.py
@@ -14,16 +14,17 @@ from utils.pretty_table import get_table
 
 router = Router()
 
-url = 'http://localhost:8000/'
+# url = 'http://localhost:8000/'
+url = 'http://web-cmc:8000/'
 
-exchanger_list = 'api/exchanger'
-exchanger_portfolio = 'api/exportfolio'
-exchanger_portfolio_data_for_user = 'api/exchanger/data/'  #  'api/exchanger/data/<int:exchanger_id>/'
+exchanger_portfolio = 'api/bot/exchanger-portfolio/'
+#  exchanger_portfolio_list - 'api/bot/exchanger-portfolio/<str:tel_username>/'
+#  exchanger_portfolio_data_for_user - 'api/bot/exchanger-portfolio/<int:exchanger_id>/<str:tel_username>/'
 
 
 @router.message(F.text == "Exchanger")
 async def exchanger(message: types.Message, state: FSMContext):
-    data = await get_exchanger()
+    data = await get_exchanger(telegram_username=message.from_user.username)
     builder_kb = kb_exchanger(data)
 
     await state.set_state(Gen.exchanger)
@@ -31,8 +32,8 @@ async def exchanger(message: types.Message, state: FSMContext):
     await message.answer('Чтобы выйти из диалога с Exchanger нажмите на кнопку ниже', reply_markup=kb_exit)
 
 
-async def get_exchanger():
-    response = requests.get(f'{url}{exchanger_portfolio}', auth=('Sergey', '!qa2ws3ed'))
+async def get_exchanger(telegram_username):
+    response = requests.get(f'{url}{exchanger_portfolio}{telegram_username}/', auth=('Sergey', '!qa2ws3ed'))
     data = response.json()
     return data
 
@@ -40,14 +41,14 @@ async def get_exchanger():
 @flags.chat_action("typing")
 @router.callback_query(F.data, Gen.exchanger)
 async def exchanger_data(clbck: CallbackQuery, state: FSMContext):
-    data = await get_exchanger_data(exchanger_id=clbck.data)
-    builder_kb = kb_exchanger(await get_exchanger())
+    data = await get_exchanger_data(exchanger_id=clbck.data, telegram_username=clbck.message.chat.username)
+    builder_kb = kb_exchanger(await get_exchanger(telegram_username=clbck.message.chat.username))
 
     await clbck.message.answer(str(data), reply_markup=builder_kb.as_markup())
 
 
-async def get_exchanger_data(exchanger_id):
-    response = requests.get(f'{url}{exchanger_portfolio_data_for_user}{exchanger_id}',
+async def get_exchanger_data(exchanger_id, telegram_username):
+    response = requests.get(f'{url}{exchanger_portfolio}{exchanger_id}/{telegram_username}',
                             auth=('Sergey', '!qa2ws3ed'))
     data = response.json()
     data = get_table(data)
