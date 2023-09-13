1fa19612231e4c0b62c40af7bf072399f84e60dd
KSerg2022 <114860122+KSerg2022@users.noreply.github.com>
2023-09-13 20:28:18 +0200

    refactored headers for request {'TEL-USERNAME': telegram_username, 'USER-PORTFOLIO-ID': exchanger_id, 'BOT-NAME': os.environ.get('BOTNAME'), 'USER-NAME': os.environ.get('USERNAME'), 'CHAT-ID': str(chat_id)}

--- handlers/exchanger.py
+++ handlers/exchanger.py
@@ -1,6 +1,7 @@
+import os
+
 import requests
 from pprint import pprint
-
 
 from aiogram import Bot, Dispatcher, types, F, Router, flags
 from aiogram.filters.command import Command
@@ -23,7 +24,8 @@ exchanger_portfolio_data = 'api/bot/exchanger-portfolio-data/'
 
 @router.message(F.text == "Exchanger")
 async def exchanger(message: types.Message, state: FSMContext):
-    data = await get_exchanger(telegram_username=message.from_user.username)
+    data = await get_exchanger(telegram_username=message.from_user.username,
+                               chat_id=message.chat.id)
     builder_kb = kb_exchanger(data)
 
     await state.set_state(Gen.exchanger)
@@ -31,8 +33,12 @@ async def exchanger(message: types.Message, state: FSMContext):
     await message.answer('Чтобы выйти из диалога с Exchanger нажмите на кнопку ниже', reply_markup=kb_exit)
 
 
-async def get_exchanger(telegram_username):
-    headers = {'TEL-USERNAME': telegram_username}
+async def get_exchanger(telegram_username, chat_id):
+    headers = {'TEL-USERNAME': telegram_username,
+               'BOT-NAME': os.environ.get('BOTNAME'),
+               'USER-NAME': os.environ.get('USERNAME'),
+               'CHAT-ID': str(chat_id),
+               }
     response = requests.get(f'{url}{exchanger_portfolio}',
                             headers=headers)
     data = response.json()
@@ -42,18 +48,24 @@ async def get_exchanger(telegram_username):
 @flags.chat_action("typing")
 @router.callback_query(F.data, Gen.exchanger)
 async def exchanger_data(clbck: CallbackQuery, state: FSMContext):
-    data = await get_exchanger_data(exchanger_id=clbck.data, telegram_username=clbck.message.chat.username)
-    builder_kb = kb_exchanger(await get_exchanger(telegram_username=clbck.message.chat.username))
+    data = await get_exchanger_data(exchanger_id=clbck.data,
+                                    telegram_username=clbck.message.chat.username,
+                                    chat_id=clbck.message.chat.id)
+    builder_kb = kb_exchanger(await get_exchanger(telegram_username=clbck.message.chat.username,
+                                                  chat_id=clbck.message.chat.id))
 
     await clbck.message.answer(str(data), reply_markup=builder_kb.as_markup())
 
 
-async def get_exchanger_data(exchanger_id, telegram_username):
+async def get_exchanger_data(exchanger_id, telegram_username, chat_id):
     headers = {'TEL-USERNAME': telegram_username,
-               'USER-PORTFOLIO-ID': exchanger_id}
+               'USER-PORTFOLIO-ID': exchanger_id,
+               'BOT-NAME': os.environ.get('BOTNAME'),
+               'USER-NAME': os.environ.get('USERNAME'),
+               'CHAT-ID': str(chat_id)
+               }
     response = requests.get(f'{url}{exchanger_portfolio_data}',
                             headers=headers)
     data = response.json()
     data = get_table(data)
     return f'<pre>{data}</pre>'
-
